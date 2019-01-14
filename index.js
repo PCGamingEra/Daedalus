9239df945b6443d4cf50475b09050e549d17381f
Unknown <jrastas@ulapland.fi>
2019-01-14 19:39:35 +0200

    added new commands

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require the discord.js module
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix, token, gamekey } = require('./config.json');
 //create a new Discord Client
 const client = new Discord.Client();
 
@@ -9,13 +9,21 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 //token needed for client login,get this at discordapp.com/developers and put it in config.json
-client.login(config.token);
+client.login(token);
 
 client.on('message', message =>{
-  console.log(message.content);
-});
 
-if (message.content === 'ping') {
+if (message.content.startsWith(`${prefix}ping`)) {
   //send back "Pong" to the channel the message was sent in
-  message.channel.send('Pong.');
-};
+message.channel.send('Pong.');
+}
+else if (message.content.startsWith(`${prefix}Catherine`)) {
+  message.channel.send('Baa Baa');
+}
+else if (message.content.startsWith(`${prefix}Lambgame`)) {
+  message.channel.send(gamekey)
+}
+else if (message.content.startsWith(`${prefix}Discord`)) {
+  message.channel.send('discord')
+}
+});
